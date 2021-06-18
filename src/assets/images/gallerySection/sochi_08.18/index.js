ec58455fb637aae6556da13fe25a2979404d7017
Vlad-coder-678 <vova.mezencev@rambler.ru>
2021-06-18 21:11:03 +0300

    deleted rest inscriptions deleted's photos

--- src/assets/images/gallerySection/sochi_08.18/index.js
+++ src/assets/images/gallerySection/sochi_08.18/index.js
@@ -1,5 +1,4 @@
 export { default as sochi_1 } from "./driver_1.jpg";
-export { default as sochi_2 } from "./driver_2.jpg";
 export { default as sochi_3 } from "./driver_3.jpg";
 export { default as sochi_4 } from "./photo_1.jpg";
 export { default as sochi_5 } from "./photo_2.jpg";
@@ -7,4 +6,3 @@ export { default as sochi_6 } from "./photo_3.jpg";
 export { default as sochi_7 } from "./photo_4.jpg";
 export { default as sochi_8 } from "./photo_5.jpg";
 export { default as sochi_9 } from "./photo_6.jpg";
-export { default as sochi_10 } from "./photo_7.jpg";
